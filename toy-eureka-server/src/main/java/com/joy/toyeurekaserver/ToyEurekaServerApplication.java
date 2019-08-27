1fe4a23f23b63657659cbe74906ac6282b12bc3e
heesunjeong <jhswit@naver.com>
2019-08-28 01:28:35 +0900

    add eureka server

---
+++ toy-eureka-server/src/main/java/com/joy/toyeurekaserver/ToyEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.joy.toyeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ToyEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyEurekaServerApplication.class, args);
+    }
+
+}
